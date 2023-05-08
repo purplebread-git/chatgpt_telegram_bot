3a26169346ee86923694493da027a3f7c7eb92ec
Balashov Dmitrii <49812139+purplebread-git@users.noreply.github.com>
2023-05-09 00:20:17 +0300

    update

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ import sqlite3
 import telebot
 from requests.exceptions import ReadTimeout
 from openai.error import RateLimitError, InvalidRequestError
-
 from dotenv import dotenv_values
 
-#import os
-#from dotenv import load_dotenv
-#env = os.path.join(os.path.dirname("/.env"), '.env')
-#if os.path.exists(env):
-#    load_dotenv(env)
+global c
+c = 0
 
 env = {
     **dotenv_values(r"C:\Users\Дима\Documents\GitHub\chatgpt_telegram_bot\.env"),
@@ -179,13 +175,16 @@ def send_start(message):
 
 @bot.message_handler(commands=["send"])
 def send_message(message):
-    conn = sqlite3.connect('db.db')
-    cursor = conn.cursor()
-    bot.send_message(message.chat.id, "Пришлите одним сообщением что зарекламить")
-    cursor.execute("SELECT chat_id FROM user")
-    results = cursor.fetchall()
-    for i in results:
-        print(i[0])
+    global c
+    if c == 0:
+        conn = sqlite3.connect('db.db')
+        cursor = conn.cursor()
+        bot.send_message(message.chat.id, "Пришлите одним сообщением что зарекламить")
+        cursor.execute("SELECT chat_id FROM user")
+        results = cursor.fetchall()
+        c = 1
+
+
 
 def check_key(message):
     key = message.text[19:]
@@ -205,13 +204,18 @@ def check_key(message):
 
 @bot.message_handler(content_types=["text"])
 def send_msg_to_chatgpt(message):
-    if CHECK_KEY == message.text[:19]:
-        check_key(message)
-        return
-    api_key_numb = 0
-    openai.api_key = API_KEYS_CHATGPT[api_key_numb]
-    write_to_db(message)
-    make_request(message, api_key_numb)
+    global c
+    if c == 0:
+        if CHECK_KEY == message.text[:19]:
+            check_key(message)
+            return
+        api_key_numb = 0
+        openai.api_key = API_KEYS_CHATGPT[api_key_numb]
+        write_to_db(message)
+        make_request(message, api_key_numb)
+    if c == 1:
+        print(message.text)
+        c = 0
 
 
 if __name__ == "__main__":
